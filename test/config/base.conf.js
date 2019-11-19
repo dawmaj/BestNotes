538db4195bbe97ed441541d9f4f6694b57361d27
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2019-11-19 22:28:07 +0100

    Update config

--- test/config/base.conf.js
+++ test/config/base.conf.js
@@ -11,7 +11,7 @@ exports.config = {
         maxInstances: 5,
         browserName: 'chrome',
         'goog:chromeOptions': {
-            args: ['--no-sandbox', '--disable-dev-shm-usage'],
+            args: ['--no-sandbox', '--disable-dev-shm-usage','--headless','--start-maximized'],
         },
     }],
 
