0085bb8c22dafa4c7740eaa7ee1a2b757421f7be
Th3NiKo <niko.pachala@onet.eu>
2019-11-27 23:08:32 +0100

    Merge models

--- Front/models.py
+++ Front/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+#Klasa opisująca zagadnienie dla danego przedmiotu
+class Zagadnienie(models.Model):
+    nazwa = models.CharField(max_length=30)
+    przedmiot = models.ForeignKey(Przedmiot, on_delete=models.CASCADE)
+    data_dodania = models.DateField()
+
 class Przedmiot(models.Model):
     nazwa = models.CharField(max_length=30)
     #uzytkownik_id = models.ForeignKey(User, on_delete=models.CASCADE)
