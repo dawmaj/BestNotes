48c4109964fc788516c2fc99e07678ea41013a85
Th3NiKo <niko.pachala@onet.eu>
2020-01-11 13:17:12 +0100

    BES-55 Show notes with tinymce plugin

---
+++ bestnotes/static/shownote.js
@@ -0,0 +1,19 @@
+//activate tinyMCE
+tinymce.init({
+    selector: '.tmpTextArea',
+    menubar: false,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu paste code'
+    ],
+    readonly: 1,
+    toolbar: false,
+    height: 500,
+    width: 700,
+});
+//get what is inside the textarea editor
+var content = $('iframe').contents().find('#tinymce').contents();
+//append it in a div
+$("#toAppend").append(content);
+//disable edit 
